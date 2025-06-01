9e3118f329ac6626df2fa2967183b9922b3b7b6f
uldiniumGit <114669125+uldiniumGit@users.noreply.github.com>
2025-06-01 17:20:09 +0700

    Update views.py

--- big_lesson/test_app/views.py
+++ big_lesson/test_app/views.py
@@ -6,74 +6,62 @@ from .forms import ContactForm
 from django.core.mail import send_mail
 
 
+def get_top_news_context():
+    news = News.objects.order_by('-id')
+    context = {}
+    if len(news) > 0:
+        context['news00'] = news[0]
+    if len(news) > 1:
+        context['news01'] = news[1]
+    if len(news) > 2:
+        context['news02'] = news[2]
+    return context
+
+
 # Главная страница
 def main_view(request):
     client = Client.objects.all()
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/index.html', context={'client': client, 'news00': news00, 'news01': news01, 'news02': news02})
+    context = {'client': client}
+    context.update(get_top_news_context())
+    return render(request, 'test_app/index.html', context=context)
 
 
 # Цены
 def show_prices(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/pricing.html', context={'news00': news00, 'news01': news01, 'news02': news02})
+    context = get_top_news_context()
+    return render(request, 'test_app/pricing.html', context=context)
 
 
 # Наши услуги. 4 вкладки
 def delivery_to_rf(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/DeliveryToRF.html', context={'news00': news00, 'news01': news01, 'news02': news02})
+    context = get_top_news_context()
+    return render(request, 'test_app/DeliveryToRF.html', context=context)
 
 
 def redemption_of_goods(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/RedemptionOfGoods.html', context={'news00': news00, 'news01': news01, 'news02': news02})
+    context = get_top_news_context()
+    return render(request, 'test_app/RedemptionOfGoods.html', context=context)
 
 
 def goods_check(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/GoodsCheck.html', context={'news00': news00, 'news01': news01, 'news02': news02})
+    context = get_top_news_context()
+    return render(request, 'test_app/GoodsCheck.html', context=context)
 
 
 def export_of_personal_belongings(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/ExportOfPersonalBelongings.html', context={'news00': news00, 'news01': news01, 'news02': news02})
+    context = get_top_news_context()
+    return render(request, 'test_app/ExportOfPersonalBelongings.html', context=context)
 
 
 # TODO страница пока не используется
-# О нас
 def about(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/about.html', context={'news00': news00, 'news01': news01, 'news02': news02})
+    context = get_top_news_context()
+    return render(request, 'test_app/about.html', context=context)
 
 
 # Оставить заявку
 def application(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
+    context = get_top_news_context()
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -82,91 +70,55 @@ def application(request):
             message = form.cleaned_data['message']
             email = form.cleaned_data['email']
 
-            # TODO пока не работает
-            a = Client()
-            a.name = name
-            a.email = email
-            a.description = message
-
-            a.save()
+            # сохранить данные в Client
+            Client.objects.create(name=name, email=email, description=message)
 
             send_mail(
                 'contact',
-                f'{name}, ваше сообщение {message} принято',
+                f'{name}, ваше сообщение "{message}" принято',
                 'uldinium@yandex.ru',
                 [email],
                 fail_silently=True,
             )
             return HttpResponseRedirect(reverse('test_app:index'))
         else:
-            return render(request, 'test_app/application.html', context={'form': form, 'news00': news00, 'news01': news01, 'news02': news02})
+            context['form'] = form
+            return render(request, 'test_app/application.html', context=context)
     else:
         form = ContactForm()
-        return render(request, 'test_app/application.html', context={'form': form, 'news00': news00, 'news01': news01, 'news02': news02})
+        context['form'] = form
+        return render(request, 'test_app/application.html', context=context)
 
 
 # TODO страница лишняя, надо убрать после того, как доделаю новую страницу со списком новостей
-
-#  Новости
 def news_list(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/news.html', context={'news00': news00, 'news01': news01, 'news02': news02})
+    context = get_top_news_context()
+    return render(request, 'test_app/news.html', context=context)
 
 
-# Контакты
 def contact(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/contact.html', context={'news00': news00, 'news01': news01, 'news02': news02})
+    context = get_top_news_context()
+    return render(request, 'test_app/contact.html', context=context)
 
 
-'''
-!!!!!!!!!!!!!!!!!! Блок с новостями !!!!!!!!!!!!!!!!!!
-'''
 
-
-# Новости 1
 def news1(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/news1.html', context={'news00': news00, 'news01': news01, 'news02': news02})
+    context = get_top_news_context()
+    return render(request, 'test_app/news1.html', context=context)
 
 
-# Новости 2
 def news2(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/news2.html', context={'news00': news00, 'news01': news01, 'news02': news02})
+    context = get_top_news_context()
+    return render(request, 'test_app/news2.html', context=context)
 
 
-# Новости 3
 def news3(request):
-    news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/news3.html', context={'news00': news00, 'news01': news01, 'news02': news02})
+    context = get_top_news_context()
+    return render(request, 'test_app/news3.html', context=context)
 
 
-# Новости еще
 def more_news(request):
     news = News.objects.order_by('-id')
-    news00 = news[0]
-    news01 = news[1]
-    news02 = news[2]
-    return render(request, 'test_app/moreNews.html', context={'news': news, 'news00': news00, 'news01': news01, 'news02': news02})
-
-
-# TODO создать четыре страницы под последние новости. Страница должна наполняться данными из списка новостей.
-# TODO создать страницу со списком всех старых новостей
-# TODO красиво оформить страницу новостей
-# TODO сделать переменную для новых новостей в моделях
+    context = {'news': news}
+    context.update(get_top_news_context())
+    return render(request, 'test_app/moreNews.html', context=context)
