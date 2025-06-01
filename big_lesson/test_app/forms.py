7a3f37650547290453b608661164276ffcf23d43
uldiniumGit <114669125+uldiniumGit@users.noreply.github.com>
2025-06-01 17:21:10 +0700

    Update forms.py

--- big_lesson/test_app/forms.py
+++ big_lesson/test_app/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 
-
 class ContactForm(forms.Form):
-
-    name = forms.CharField(label='название')
-    email = forms.EmailField(label='email')
-    message = forms.CharField(label='сообщение')
+    name = forms.CharField(
+        label='Имя',
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Ваше имя'})
+    )
+    email = forms.EmailField(
+        label='Email',
+        max_length=100,
+        widget=forms.EmailInput(attrs={'placeholder': 'example@mail.com'})
+    )
+    message = forms.CharField(
+        label='Сообщение',
+        widget=forms.Textarea(attrs={'placeholder': 'Ваше сообщение', 'rows': 4})
+    )
