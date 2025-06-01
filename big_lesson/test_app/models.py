178659b03326b2ecad76a1d1af6a4be15570d61b
uldiniumGit <114669125+uldiniumGit@users.noreply.github.com>
2025-06-01 17:20:52 +0700

    Update models.py

--- big_lesson/test_app/models.py
+++ big_lesson/test_app/models.py
@@ -1,46 +1,32 @@
 from django.db import models
 
-# Create your models here.
-
 
 class City(models.Model):
-    name = models.CharField(max_length=32, unique=True)
+    name = models.CharField(max_length=32, unique=True, verbose_name='Название города')
 
     def __str__(self):
         return self.name
 
 
 class Type(models.Model):
-    type = models.CharField(max_length=32, unique=True)
+    type = models.CharField(max_length=32, unique=True, verbose_name='Тип')
 
     def __str__(self):
         return self.type
 
 
 class Client(models.Model):
-    name = models.CharField(max_length=32, unique=True)
-    description = models.TextField()
-    email = models.EmailField(max_length=32, unique=True)
-
-    # Связь с категорией
-    # Один - много
-    # email = models.ForeignKey(Email, on_delete=models.CASCADE)
-
-    # Много - много
-    # city = models.ManyToManyField(City, null=True, blank=True)
-
-    # type = models.ManyToManyField(Type, null=True, blank=True)
-
-    # Картинка
-    # image = models.ImageField(upload_to='clients', null=True, blank=True)
+    name = models.CharField(max_length=32, verbose_name='Имя клиента')
+    description = models.TextField(verbose_name='Описание')
+    email = models.EmailField(max_length=32, unique=True, verbose_name='Email')
 
     def __str__(self):
         return self.name
 
 
 class News(models.Model):
-    news_name = models.CharField(max_length=32, unique=False)
-    news_text = models.CharField(max_length=512, unique=False)
+    news_name = models.CharField(max_length=32, verbose_name='Заголовок новости')
+    news_text = models.TextField(verbose_name='Текст новости')
 
     def __str__(self):
         return self.news_name
